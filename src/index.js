a1dd1f3404ac639e45fc89877607967ce0bed57a
ROBIN Jonathan <jonathan@levelup-analytics.com>
2022-11-14 20:31:29 +0400

    adding router

--- src/index.js
+++ src/index.js
@@ -3,11 +3,33 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import Root from './routes/root';
+import NotFound from './scenes/notFound/notFound';
+import About from './scenes/About/about';
+
+
+const router = createBrowserRouter([ 
+  {
+    path: "/", 
+    element: <Root />,
+    errorElement: <NotFound />,
+  }, 
+  { 
+    path: "/about", 
+    element: <About />, 
+    errorElement: <NotFound />,
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
