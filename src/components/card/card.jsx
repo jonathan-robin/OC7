f8a965f0ff2b7a0cb8312ec476c505e6bc3436d8
ROBIN Jonathan <jonathan@levelup-analytics.com>
2022-11-15 11:52:33 +0400

    handleOnClick on card

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -2,8 +2,13 @@ import { render } from "@testing-library/react";
 import './Card.css';
 
 export default function Card(props){ 
+
+    function handleOnClickCard(){ 
+        console.log(props.id);
+    }
+
     return (
-        <div className="container__card">
+        <div className="container__card" onClick={handleOnClickCard}>
             <div className="container__card-image" style={{background: 'url("'+props.cover+'")'}}>
                 {/* <img src={props.cover} alt={props.description} className="card-image" /> */}
             </div>
