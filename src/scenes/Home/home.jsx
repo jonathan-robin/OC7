52a0e92a80784ee552c4887a3e4ac4f5f962bc37
ROBIN Jonathan <jonathan@levelup-analytics.com>
2022-11-15 09:34:50 +0400

    init Header comp

--- src/scenes/Home/home.jsx
+++ src/scenes/Home/home.jsx
@@ -1,13 +1,15 @@
 import { useEffect } from "react";
 import json from '../../json/logements.json';
-
-
+import Header from '../../components/header/Header';
 
 export default function Home(props){ 
 
-return (<div>
+return (
+    <div className="container__home">
+        <Header />
        {json.map(logement => { 
             return <div>{logement.id}</div>
         })}
-    </div>)
+    </div>
+    )
 }
