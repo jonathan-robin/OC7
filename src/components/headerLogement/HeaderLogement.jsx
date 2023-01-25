1936ca62aec85a36239a1aa7394eb50c47c593d9
ROBIN Jonathan <jonathan@levelup-analytics.com>
2023-01-25 21:20:34 +0400

    use css var

--- src/components/headerLogement/HeaderLogement.jsx
+++ src/components/headerLogement/HeaderLogement.jsx
@@ -24,19 +24,19 @@ export default function HeaderLogement(props){
                     </div>
                     <div className="container__stars">
                         <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M18.645 12L15 0L11.355 12H0L9.27 18.615L5.745 30L15 22.965L24.27 30L20.745 18.615L30 12H18.645Z" fill={props.rating > 0 ? '#FF6060' : "#E3E3E3"}/>
+                            <path d="M18.645 12L15 0L11.355 12H0L9.27 18.615L5.745 30L15 22.965L24.27 30L20.745 18.615L30 12H18.645Z" fill={props.rating > 0 ? 'var(--color-primary)' : "#E3E3E3"}/>
                         </svg>
                             <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M18.645 12L15 0L11.355 12H0L9.27 18.615L5.745 30L15 22.965L24.27 30L20.745 18.615L30 12H18.645Z" fill={props.rating > 1 ? '#FF6060' : "#E3E3E3"}/>
+                            <path d="M18.645 12L15 0L11.355 12H0L9.27 18.615L5.745 30L15 22.965L24.27 30L20.745 18.615L30 12H18.645Z" fill={props.rating > 1 ? 'var(--color-primary)' : "#E3E3E3"}/>
                         </svg>
                             <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M18.645 12L15 0L11.355 12H0L9.27 18.615L5.745 30L15 22.965L24.27 30L20.745 18.615L30 12H18.645Z" fill={props.rating > 2 ? '#FF6060' : "#E3E3E3"}/>
+                            <path d="M18.645 12L15 0L11.355 12H0L9.27 18.615L5.745 30L15 22.965L24.27 30L20.745 18.615L30 12H18.645Z" fill={props.rating > 2 ? 'var(--color-primary)' : "#E3E3E3"}/>
                         </svg>
                             <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M18.645 12L15 0L11.355 12H0L9.27 18.615L5.745 30L15 22.965L24.27 30L20.745 18.615L30 12H18.645Z" fill={props.rating > 3 ? '#FF6060' : "#E3E3E3"}/>
+                            <path d="M18.645 12L15 0L11.355 12H0L9.27 18.615L5.745 30L15 22.965L24.27 30L20.745 18.615L30 12H18.645Z" fill={props.rating > 3 ? 'var(--color-primary)' : "#E3E3E3"}/>
                         </svg>
                             <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M18.645 12L15 0L11.355 12H0L9.27 18.615L5.745 30L15 22.965L24.27 30L20.745 18.615L30 12H18.645Z" fill={props.rating > 4 ? '#FF6060' : "#E3E3E3"}/>
+                            <path d="M18.645 12L15 0L11.355 12H0L9.27 18.615L5.745 30L15 22.965L24.27 30L20.745 18.615L30 12H18.645Z" fill={props.rating > 4 ? 'var(--color-primary)' : "#E3E3E3"}/>
                         </svg>
                     </div>
                 </div>
