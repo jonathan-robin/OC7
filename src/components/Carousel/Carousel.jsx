0e179bc50ac6e71a7a5d76e702965e13c74b78ee
ROBIN Jonathan <jonathan@levelup-analytics.com>
2023-01-26 21:54:00 +0400

    pagination images

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,27 +1,34 @@
 
 import './Carousel.css';
 import React, { useState } from 'react';
+import { useEffect } from 'react';
 
 // Function for the About page that displays title + description
 export default function Carousel({ pictures, description }){ 
 
     let hasManyPictures = pictures.length > 1 ? true : false;
     let curSlide = 0;
+    const [count, setCount] = useState(0);
     let maxSlide = pictures.length - 1;
     const [nbPictureLoaded, setNbPictureLoaded] = useState(0);
+
+    useEffect(() => {
+        setCount(curSlide);
+    },[curSlide])
 
     function handleClickNext(){ 
         if (curSlide === maxSlide) curSlide = 0;
         else curSlide++; 
-        var arr = [].slice.call(document.getElementsByClassName("container__picture"));
-        arr.forEach((slide, indx) => slide.style.transform = `translateX(${100 * (indx - curSlide)}%)` );
+        [].slice.call(document.getElementsByClassName("container__picture")).forEach((slide, indx) => slide.style.transform = `translateX(${100 * (indx - curSlide)}%)` );
+        [].slice.call(document.getElementsByClassName("number-images")).forEach((slide, indx) => slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`);
     }
 
     function handleClickPrev(){ 
         if (curSlide === 0) curSlide = maxSlide;
         else curSlide--; 
-        var arrPrev = [].slice.call(document.getElementsByClassName("container__picture"));
-        arrPrev.forEach((slide, indx) => slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`);
+        [].slice.call(document.getElementsByClassName("container__picture")).forEach((slide, indx) => slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`);
+        [].slice.call(document.getElementsByClassName("number-images")).forEach((slide, indx) => slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`);
+
     }
 
     const pictureLoaded = () => {
@@ -34,13 +41,14 @@ export default function Carousel({ pictures, description }){
                 <div className="lds-dual-ring"></div>
                 <div className="container__pictures">
                     {pictures && pictures.map((picture, index) => { 
-                        return <img src={picture} onLoad={pictureLoaded} style={{transform: 'translateX('+index*100+"%)"}} className="container__picture" key={index} alt={"Photo n°"+index + 1+" du logement"+description}/>
+                        return <div key={index}><img src={picture} onLoad={pictureLoaded} style={{transform: 'translateX('+index*100+"%)"}} className="container__picture" alt={"Photo n°"+index + 1+" du logement"+description}/>
+                        <div style={{transform: 'translateX('+index*100+"%)"}} className="number-images">{index + 1} / { maxSlide + 1 }</div></div>
                     })}
                 </div>
                 {hasManyPictures &&            
                 <div> 
-                    <div onClick={handleClickPrev} className="btn btn-prev">&#60;</div>
-                    <div onClick={handleClickNext} className="btn btn-next">&#62;</div>
+                    <div onClick={() => handleClickPrev()} className="btn btn-prev">&#60;</div>
+                    <div onClick={() => handleClickNext()} className="btn btn-next">&#62;</div>
                 </div>
                 }
      
