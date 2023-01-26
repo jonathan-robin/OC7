5b401678cf71b433dc09746d6337a66d0d8f0537
ROBIN Jonathan <jonathan@levelup-analytics.com>
2023-01-26 20:44:30 +0400

    factor

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,14 +1,14 @@
 import './Card.css';
 import { useNavigate } from 'react-router-dom';
 
-export default function Card(props){ 
+export default function Card({logement}){ 
     let navigation = useNavigate();
-    const handleOnClickCard = () =>  navigation('/fiche-logement/'+props.id)
+    const handleOnClickCard = () =>  navigation('/fiche-logement/'+logement.id); 
 
     return (
         <div className="container__card" onClick={handleOnClickCard}>
-            <div className="container__card-title"><p>{props.title}</p></div>
-            <div className="container__card-image" style={{background: 'url("'+props.cover+'")'}}></div>
+            <div className="container__card-title"><p>{logement.title}</p></div>
+            <div className="container__card-image" style={{background: 'url("'+logement.cover+'")'}}></div>
         </div>
     )
 
--- src/scenes/Home/home.jsx
+++ src/scenes/Home/home.jsx
@@ -13,11 +13,7 @@ export default function Home(){
                 <Header />
                 <Banner customClass="banner-home" path='bannerhome.png' sentence="Chez vous, partout et ailleurs" alt="bannière pour la page accueil d'un paysage côtier brumeux"/>
                 <div className="container__cards">
-                    {jsonLogement.map(logement => { 
-                        return <Card key={logement.id} cover={logement.cover} description={logement.description} title={logement.title} id={logement.id}
-                            pictures={logement.pictures} host={logement.host} rating={logement.rating} location={logement.location} equipments={logement.equipments} tags={logement.tags }
-                            />
-                    })}
+                    {jsonLogement.map(logement => <Card key={logement.id} logement={logement} /> )}
                 </div>
             </div>
             <Footer />
